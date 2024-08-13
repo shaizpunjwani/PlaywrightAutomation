c32a299f49817eb6701de88b7a4f41df84597f70
Shaiz Akber <77119829+shaizpunjwani@users.noreply.github.com>
2024-08-13 16:27:01 +0500

    Added parametrization and playwright fixtures

--- Testing/EndFlow.spec.js
+++ Testing/EndFlow.spec.js
@@ -1,26 +1,29 @@
 const {test, expect} = require('@playwright/test');
 const { POManager } = require('../PageObjects/POManager.spec');
+const {customtest} = require("../Utlis/test-base");
 
 const Testdata = JSON.parse(JSON.stringify(require("../Utlis/Testdata.json")));
 
+for(const data of Testdata)
+{
 
-test("end to end flow of adding items to cart", async({page})=>{
+test(`End to End flow testing of product ${data.prodname}`, async({page})=>{
 
     const pomanager= new POManager(page);
     await page.goto("https://rahulshettyacademy.com/client/");
     await page.waitForLoadState("domcontentloaded");
     const loginpage=pomanager.GetLoginPage();
 
-    await loginpage.Login(Testdata.email, Testdata.pwd);
+    await loginpage.Login(data.email, data.pwd);
 
     const dashboardpage=pomanager.GetDashboardPage()
 
-    await dashboardpage.Search_Add_Products(Testdata.prodname);
+    await dashboardpage.Search_Add_Products(data.prodname);
     await dashboardpage.Navigate_Cart();
 
     const cartpage = pomanager.GetCartPage();
 
-    await cartpage.Check_Product_Visibility('IPHONE 13 PRO');
+    await cartpage.Check_Product_Visibility(data.prodname);
     await cartpage.Navigate_CheckoutPage();
 
     const orderinfo = pomanager.GetOrderInfoPage();
@@ -35,3 +38,25 @@ test("end to end flow of adding items to cart", async({page})=>{
 
     
 });
+};
+
+
+customtest.only("Using playwright fixture", async({page, TestData})=>{
+
+    const pomanager= new POManager(page);
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.waitForLoadState("domcontentloaded");
+    const loginpage=pomanager.GetLoginPage();
+
+    await loginpage.Login(TestData.email, TestData.pwd);
+
+    const dashboardpage=pomanager.GetDashboardPage()
+
+    await dashboardpage.Search_Add_Products(TestData.prodname);
+    await dashboardpage.Navigate_Cart();
+
+    const cartpage = pomanager.GetCartPage();
+
+    await cartpage.Check_Product_Visibility(TestData.prodname);
+    await cartpage.Navigate_CheckoutPage();
+})
